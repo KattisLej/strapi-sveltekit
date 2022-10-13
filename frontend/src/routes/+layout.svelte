30664be6eea3ad713993b1f9b528438cfeda45ea
Henrike <henrike.wiemker@t-online.de>
2022-10-13 13:59:48 +0200

    wipe demo content and replace with custom starter

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,53 +1,20 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
-</div>
+<header>
+  This is a beautiful header for every page. It lives in the +layout file.
+</header>
+<main>
+  <slot />
+</main>
+<footer class="footer">
+  Also, all pages will have this nice footer. Even that comes from +layout.
+</footer>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+  .footer {
+    position: absolute;
+    bottom: 10px
+  }
 </style>
